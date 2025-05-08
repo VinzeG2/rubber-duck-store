a794e421bc5632c0b7bad9c4e32c746fec763fe6
VinzeG2 <vinze.g2@gmail.com>
2025-05-07 22:56:45 -0400

    orders endpoint now shows packaging type and filling

--- src/server/routes/orders.js
+++ src/server/routes/orders.js
@@ -33,7 +33,7 @@ router.post("/", async (req, res) => {
         quantity,
         country,
         deliveryMode,
-        package: getPackageType,
+        package: packageType,
         filling: fillingType,
     })
 
